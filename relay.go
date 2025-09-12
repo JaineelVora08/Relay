775f8a3c846b6df3774bfcf1540b843aac8e15b5
Aradhya Mahajan <147337970+Aradhya2708@users.noreply.github.com>
2025-09-12 23:44:07 +0530

    Update relay.go

--- relay.go
+++ relay.go
@@ -148,7 +148,7 @@ func main() {
 
 	RelayHost, err = libp2p.New(
 		libp2p.Identity(privKey),
-		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/443/ws"),
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/8080/ws"),
 		libp2p.Security(libp2ptls.ID, libp2ptls.New),
 		libp2p.ConnectionManager(connMgr),
 		libp2p.EnableNATService(),
