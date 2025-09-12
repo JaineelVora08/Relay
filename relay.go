c26f432cae809a1e599eac1848fb7434509ecc89
Aradhya Mahajan <147337970+Aradhya2708@users.noreply.github.com>
2025-09-13 00:59:41 +0530

    auto

--- relay.go
+++ relay.go
@@ -127,6 +127,15 @@ func main() {
 	if port == "" {
 		port = "443"
 	}
+	secret := os.Getenv("RELAY_KEY_STRING")
+	if secret == "" {
+		panic("RELAY_KEY_STRING environment variable not set")
+	}
+	// 2. Create a deterministic private key
+	privKey, err := deterministicKeyFromString(secret)
+	if err != nil {
+		panic(err)
+	}
 	if JS_API_key == "" || JS_ServerURL == "" {
 		fmt.Println("[DEBUG] Missing JS API key or server URL")
 		return
@@ -139,11 +148,11 @@ func main() {
 		log.Fatalf("[ERROR] Failed to create connection manager: %v", err)
 	}
 
-	privKey, _, err := crypto.GenerateEd25519Key(rand.Reader)
-	if err != nil {
-		// handle error
-		panic(err)
-	}
+	// privKey, _, err := crypto.GenerateEd25519Key(rand.Reader)
+	// if err != nil {
+	// 	// handle error
+	// 	panic(err)
+	// }
 	fmt.Println("[DEBUG] Creating relay host...")
 
 	RelayHost, err = libp2p.New(
@@ -235,6 +244,11 @@ func remove(Lists *[]string, val string) {
 			return
 		}
 	}
+}
+
+func deterministicKeyFromString(secret string) (crypto.PrivKey, error) {
+	sum := sha256.Sum256([]byte(secret))
+	return crypto.Ed25519PrivateKeyFromSeed(sum[:])
 }
 
 func hourlyResetLoop() {
